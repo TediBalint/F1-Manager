cf75ae92feea04d7c5afde1e46d574635ad555d2
TediBalint <tedi.balint@students.jedlik.eu>
2023-10-17 11:32:08 +0200

    PlayView

---
+++ F1 Manager/Viewmodels/PlayViewModel.cs
@@ -0,0 +1,23 @@
+﻿using F1_Manager.Commands;
+using F1_Manager.Stores;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace F1_Manager.Viewmodels
+{
+    public class PlayViewModel : ViewModelBase
+    {
+        public string WelcomeMessage => "Play";
+        public ICommand NavigatePlayCommand { get; }
+
+        public PlayViewModel(NavigationStore navigatedStore)
+        {
+            NavigatePlayCommand = new NavigateHomeCommand(navigatedStore);
+        }
+    }
+}
